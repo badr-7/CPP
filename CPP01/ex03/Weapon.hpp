ae29ffb2fd5aeba424720173df1552429a2bd0d6
Mohammed badr-eddine El housni <mel-hous@e2r2p11.1337.ma>
2023-03-09 17:49:15 +0100

    DONE Thu Mar  9 17:49:15 +01 2023

--- CPP01/ex03/Weapon.hpp
+++ CPP01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 10:25:39 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/02/20 11:32:12 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/09 17:40:46 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ class Weapon {
         std::string _weapon;
     public :
         Weapon(std::string name);
+        ~Weapon(){};
         const std::string    &getType() const;
         void    setType(std::string name);        
 };
