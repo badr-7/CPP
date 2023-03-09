9cea0ece7d13ad777f28b2ace8196fece72b301e
Mohammed badr-eddine El housni <mel-hous@e2r2p11.1337.ma>
2023-03-09 17:49:49 +0100

    DONE Thu Mar  9 17:49:49 +01 2023

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:58:25 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/02/21 11:38:57 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/09 17:43:21 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int main(int ac, char **av) 
 {
-    if (ac < 4 || ac > 4)
+    if (ac != 4)
         std::cout << "ARGUMENTS NOT VALID" << std::endl;
     else
     {
@@ -32,22 +32,24 @@ int main(int ac, char **av)
         std::string line;
         s.append(".replace");
         std::ofstream file_to_store(s);
+        std::getline(file_to_read,line,'\0');
         if(!file_to_store)
         {
             std::cout << "ERROR : The file can't be create"<< std::endl;
             return(1);
         }
-        while(std::getline(file_to_read,line)){
-            int position = -1;
+        if (s2 == s3)
+            file_to_store << line;
+        else{
+            unsigned long position = 0;
             position = line.find(s2);
-            if(position != std::string::npos)
+            while(position != std::string::npos)
             {
                 line.erase(position,s2.length());
                 line.insert(position,s3);
-                file_to_store << line.append("\n");
+                position = line.find(s2);
             }
-            else
-                file_to_store << line.append("\n");
+            file_to_store << line;
         }
         file_to_store.close();
         file_to_read.close();
