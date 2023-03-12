6392e9dce0474a4c76896b3436e44a71c52ef195
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-03-12 14:20:07 +0100

    DONE Sun Mar 12 14:20:07 +01 2023

--- CPP02/ex02/Fixed.cpp
+++ CPP02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 13:39:48 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/03/12 13:45:26 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/12 13:59:55 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,25 @@
         Fixed::Fixed(const Fixed &other){
             this->_fixedpoint = other.getRawBits();
         };
-        Fixed::Fixed::Fixed(const int i){
-            
+        Fixed::Fixed(const int i){
+            this->_fixedpoint = i << 1;
         };
         Fixed::Fixed(const float f){
-            
+            this->_fixedpoint = roundf(f * 256);
         };
         int Fixed::getRawBits( void ) const{
             return(this->_fixedpoint);
         };
         float Fixed::toFloat( void ) const{
-            
+            return((float)_fixedpoint / 256);
         };
         int Fixed::toInt( void ) const{
-            
+            return(_fixedpoint / 256);
         };
         void setRawBits(int const raw);
         Fixed& Fixed::operator=(const Fixed &obj2){
-            
+            this->_fixedpoint = obj2.getRawBits();
+            return(*this);
         };
         Fixed& Fixed::operator>(const Fixed &obj2){
             
