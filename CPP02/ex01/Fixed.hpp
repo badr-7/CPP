15292a79c4d6494715c277bf371c02700654d63e
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-03-12 13:33:54 +0100

    DONE Sun Mar 12 13:33:53 +01 2023

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,11 +6,12 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 18:44:28 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/03/09 18:53:46 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/11 18:02:35 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <math.h>
 
 class Fixed{
     private:
@@ -18,10 +19,16 @@ class Fixed{
         static const int _fractionalbits = 8;
     public:
         Fixed();
-        Fixed(Fixed &i);
+        Fixed(const Fixed &other);
+        Fixed(const int i);
+        Fixed(const float f);
         int getRawBits( void ) const;
         float toFloat( void ) const;
         int toInt( void ) const;
         void setRawBits(int const raw);
+        Fixed& operator=(const Fixed &obj2);
         ~Fixed();
+        
 };
+
+std::ostream&   operator<<(std::ostream &out, const Fixed &fixed);
