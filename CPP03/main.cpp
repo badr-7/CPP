14f52618be7a6e93e0a6907e16149798a646c47c
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-03-14 13:52:05 +0100

    DONE Tue Mar 14 13:52:05 +01 2023

---
+++ CPP03/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 13:04:45 by mel-hous          #+#    #+#             */
+/*   Updated: 2023/03/14 13:51:34 by mel-hous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main()
+{
+
+	ClapTrap trap("badr");
+	ClapTrap clap(trap);
+    
+
+	std::cout << std::endl;
+	
+	clap.attack("said");
+	clap.takeDamage(0);
+	clap.beRepaired(20);
+	
+	std::cout << std::endl;
+	
+	clap.attack("simo");
+	clap.takeDamage(20);
+	clap.beRepaired(5);
+	
+	std::cout << std::endl;
+
+	return 0;
+}
