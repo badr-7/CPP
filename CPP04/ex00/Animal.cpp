bbaf1b1f7d22c37a0a32a0129c9aa231146b8d1e
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-03-16 16:55:33 +0100

    DONE Thu Mar 16 16:55:33 +01 2023

--- CPP04/ex00/Animal.cpp
+++ CPP04/ex00/Animal.cpp
@@ -6,11 +6,11 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 14:08:17 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/03/15 15:21:39 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/16 10:23:33 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include"Animal.hpp"
 
 // Dog dog;
 // dog.makeSound(); // dog sound
@@ -20,3 +20,23 @@
 // Animal x = dog;
 // [                           ]
 // [                                            ]
+Animal::Animal(){
+    std::cout<< "Animal Default constructor called"<<std::endl;
+};
+Animal::Animal(Animal &other){
+    std::cout<< "Animal Copy constructor called"<<std::endl;
+    *this = other;
+};
+
+std::string Animal::getType() const{
+    return this->type;
+};
+
+Animal &Animal::operator=(const Animal& other){
+    this->type = other.type;
+    return *this;
+};
+
+Animal::~Animal(){
+    std::cout<<"Animal Destructor called"<<std::endl;
+};
---
+++ CPP04/ex00/Cat.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 16:45:19 by mel-hous          #+#    #+#             */
+/*   Updated: 2023/03/16 10:24:38 by mel-hous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "Animal.hpp"
+
+class Cat : public Animal{
+    public :
+        Cat();
+        Cat(Cat &other);
+        void makeSound() const;
+        std::string getType() const;
+        Cat& operator=(const Cat &other);
+        ~Cat();
+};
