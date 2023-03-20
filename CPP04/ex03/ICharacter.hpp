4b9f023e612e5772e0b9b65cc2001371099a40da
Mohammed badr-eddine El housni <mel-hous@e2r8p15.1337.ma>
2023-03-20 15:52:49 +0000

    DONE Mon Mar 20 15:52:49 +00 2023

---
+++ CPP04/ex03/ICharacter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 13:19:17 by mel-hous          #+#    #+#             */
+/*   Updated: 2023/03/20 13:24:30 by mel-hous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include "AMateria.hpp"
+
+class ICharacter
+{
+    public:
+        virtual ~ICharacter() {}
+        virtual std::string const & getName() const = 0;
+        virtual void equip(AMateria* m) = 0;
+        virtual void unequip(int idx) = 0;
+        virtual void use(int idx, ICharacter& target) = 0;
+};
