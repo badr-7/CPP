36233bbf8665fc8351336c16975d384d17e03063
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-03-21 18:47:19 +0000

    DONE Tue Mar 21 18:47:19 +00 2023

--- CPP04/ex03/AMateria.cpp
+++ CPP04/ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:42:36 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/03/21 15:58:21 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/21 16:29:41 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 
 AMateria::AMateria(){
 
-};
+}
 AMateria::AMateria(const AMateria &){
 
-};
+}
 AMateria::~AMateria(){
     
-};
+}
 AMateria & AMateria::operator = (const AMateria &){
     return *this;
-};
+}
 
 AMateria::AMateria(std::string const & type){
     this->_type = type;
-};
+}
 
 std::string const & AMateria::getType() const{
     return _type;
-};
+}
 // virtual void use(ICharacter& target){
     
 // };
--- CPP04/ex03/Ice.cpp
+++ CPP04/ex03/Ice.cpp
@@ -6,29 +6,30 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 13:51:24 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/03/21 15:58:36 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/03/21 16:29:29 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"Ice.hpp"
 
-Ice::Ice(){
+Ice::Ice()
+{
      _type = "ice";
-};
+}
 Ice::Ice(const Ice &other){
     _type = "ice";
     *this = other;
-};
+}
 Ice::~Ice(){
     
-};
+}
 Ice & Ice::operator = (const Ice &){
    return *this;
-};
+}
 AMateria* Ice::clone() const{
     Ice* other = new Ice();
     return other; 
-};
+}
 void Ice::use(ICharacter& target){
     std::cout<< "* shoots an ice bolt at "<< target.getName() << " *"<< std::endl;
-};
+}
