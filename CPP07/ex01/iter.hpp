56f1a928192c70954b2532c85126170037ca9fe5
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-07-07 11:21:34 +0100

    Fri Jul  7 11:21:34 +01 2023

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:06:12 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/07/03 17:14:35 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/07/07 11:15:02 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,12 @@ template <typename T>
 void iter(T *a, int len, void (*f)(T &c))
 {
     for (int i = 0; i < len; i++)
-    {
         f(a[i]);
-    }
+}
+
+template <typename T>
+void iter(T *a, int len, void (*f)(const T &c))
+{
+    for (int i = 0; i < len; i++)
+        f(a[i]);
 }
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,59 +6,43 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:33:23 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/07/06 17:32:23 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/07/07 10:26:30 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-#define MAX_VAL 750
 int main(int, char**)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
+    Array<char> str(12);
+    char *s = strdup("hello world");
+    Array<int> tab(12);
+    for (int i = 0; i < 12; i++)
     {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
+        str[i] = s[i];
+        tab[i] = i;
     }
-    //SCOPE
+    for (int i = 0; i < 12; i++)
     {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
+        std::cout<<"str: "<< str[i] << "   tab: " << tab[i] << std::endl;
     }
     try
     {
-        numbers[-2] = 0;
+        std::cout<<"str: "<< str[15] << "   tab: " << tab[15] << std::endl;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
     }
     try
     {
-        numbers[MAX_VAL] = 0;
+        std::cout<<"str: "<< str[10] << "   tab: " << tab[-1] << std::endl;
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
     }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
+    
+    
     return 0;
 }
