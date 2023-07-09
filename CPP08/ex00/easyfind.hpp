44a8284602a6d7d178fee36df78a4213789b0794
Mohammed badr-eddine El housni <mel-hous@e2r2p15.1337.ma>
2023-07-09 09:08:05 +0100

    Sun Jul  9 09:08:04 +01 2023

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,21 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 17:58:01 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/07/04 17:58:02 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/07/09 09:04:36 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <iostream>
+
+template <typename T>
+void easyfind(T &arg, int i)
+{
+    typename T::iterator a = std::find(arg.begin(), arg.end(), i);
+    if (a != arg.end())
+    {
+        std::cout << "Value fond" << std::endl;
+        return;
+    }
+    throw std::runtime_error("Value not found");
+}
