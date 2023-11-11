bcdaaba4d9b94127fc425b4483e0dfb49393e774
Mohammed badr-eddine El housni <elhousnibadr5@gmail.com>
2023-11-11 10:52:48 +0100

    Sat Nov 11 10:52:48 +01 2023

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,15 +6,21 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:02:48 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/11/06 16:27:25 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/11/11 10:51:01 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"Span.hpp"
+#include <stdlib.h>
+#include <time.h>
 
 
 /////////////////////////////////////////////////////////////////////[CONSTRUCTORS]
-
+Span::Span(){
+}
+Span::Span(const Span &obj){
+        *this = obj;
+}
 
 Span::Span(unsigned int i) : size(i) {
 
@@ -37,6 +43,16 @@ Span& Span::operator=(const Span& other){
 
 /////////////////////////////////////////////////////////////////////[FUNCTIONS]
 
+unsigned int distance(std::vector<int>::iterator a, std::vector<int>::iterator b){
+    unsigned int i = 0;
+
+    while (a != b){
+        a++;
+        i++;
+    }
+    return i;
+}
+
 bool cmp(int a, int b) {
     return a < b;
 }
@@ -44,13 +60,16 @@ bool cmp(int a, int b) {
 void Span::addNumber(unsigned int i){
     if(this->numbers.size() < this->size)
         numbers.push_back(i);
+    else{
+        throw std::runtime_error("No Space Left");
+    }
 }
 unsigned int Span::shortestSpan(){
     int smallest = -1;
     int tmp = 0;
     std::vector<int> tmp_numbers = numbers;
     if (this->numbers.size() < 2){
-        throw std::runtime_error("");
+        throw std::runtime_error("No Span Can Be Found");
     }
     std::sort(tmp_numbers.begin(), tmp_numbers.end(), cmp);
     for(unsigned int i = 0; i < tmp_numbers.size(); i++)
@@ -63,9 +82,19 @@ unsigned int Span::shortestSpan(){
 }
 unsigned int Span::longestSpan(){
     if (this->numbers.size() < 2){
-        throw std::runtime_error("");
+        throw std::runtime_error("No Span Can Be Found");
     }
     return(*std::max_element(numbers.begin(), numbers.end()) - *std::min_element(numbers.begin(), numbers.end()));
+}
+void Span::fillspan(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+    if(this->size - this->numbers.size() >= distance(begin,end)){
+        for (; begin != end; begin++)
+            this->addNumber(*begin);
+    }
+    else{
+        throw std::runtime_error("No Space Left");
+    }
 }
 
 
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 15:02:51 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/07/09 14:29:33 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/11/11 10:52:35 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,13 @@ private:
     unsigned int size;
 
 public:
+    Span();
+	Span(const Span &);
     Span(unsigned int);
     Span &operator=(const Span &);
     void addNumber(unsigned int);
     unsigned int shortestSpan();
     unsigned int longestSpan();
+    void fillspan(std::vector<int>::iterator begin, std::vector<int>::iterator end);
     ~Span();
 };
