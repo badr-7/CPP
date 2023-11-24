5e89df393b789c5a1592296598c3d63084f8f5a3
Mohammed badr-eddine El housni <elhousnibadr5@gmail.com>
2023-11-24 02:12:33 +0100

    Fri Nov 24 02:12:33 +01 2023

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hous <mel-hous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:57:56 by mel-hous          #+#    #+#             */
-/*   Updated: 2023/11/16 10:18:56 by mel-hous         ###   ########.fr       */
+/*   Updated: 2023/11/23 22:55:43 by mel-hous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@ int main(int ac, char **av)
     std::map<std::string,float> data_base;
     std::map<std::string,float> input_data;
     if(ac == 2){
-        std::string input = av[1];
-        std::ifstream filetoread(input); 
-        if (!filetoread.is_open())
-	    	throw std::runtime_error("could not open file.");
+        std::ifstream filetoread(av[1]);
+        if (!filetoread.is_open()){
+	    	std::cout<<"Error: could not open file.\n";
+            return 1;
+        }
         data_base = storedata();
         process(filetoread, data_base);
     }
     else
-       throw std::runtime_error("Error: could not open file.");
+       std::cout<<"Error: could not open file.\n";
     return 0;
 }
